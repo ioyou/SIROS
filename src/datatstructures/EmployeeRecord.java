c5e45ee6616840f0474a240e91599a3de6f91225
Chris Niese <chris.niesel@gmail.com>
2011-05-28 17:03:37 -0400

    Added: -Record constructor -EmployeeRecord class Removed: -None

---
+++ src/datatstructures/EmployeeRecord.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datatstructures;
+
+/**
+ *
+ * @author Chris
+ */
+public class EmployeeRecord extends Record {
+
+    private String firstName;
+    private String lastName;
+    private int employeeNumber;
+    private String password;
+
+    public EmployeeRecord(String fname, String lname, int number, String pass) {
+        firstName = fname;
+        lastName = lname;
+        employeeNumber = number;
+        password = pass;
+    }
+
+    public EmployeeRecord() {
+        firstName = "";
+        lastName = "";
+        employeeNumber = 0;
+        password = "";
+    }
+
+    public String getRecord() {
+        String number = Integer.toString(employeeNumber);
+        String record = (firstName + " " + lastName + " " + number);
+        return record;
+    }
+
+    public void setNull() {
+        firstName = "";
+        lastName = "";
+        employeeNumber = -1;
+        password = "";
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPass() {
+        return password;
+    }
+
+    public void setFirstName(String fName) {
+        firstName = fName;
+    }
+
+    public void setLastName(String lName) {
+        lastName = lName;
+    }
+
+    public void setEmployeeNumber(int n) {
+        employeeNumber = n;
+    }
+
+    public void setPass(String hash) {
+        password = hash;
+    }
+}
---
+++ src/datatstructures/Record.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datatstructures;
+
+/**
+ *
+ * @author Chris
+ */
+abstract class Record {
+
+    abstract String getRecord();
+
+    public abstract void setNull();
+}
