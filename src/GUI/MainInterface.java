e9cfdc91c4c5c73c7f783e4e9fd5ba6d8a35f671
Chris Niesel <chris.niesel@gmail.com>
2011-05-29 12:48:42 -0400

    Added: -manager boolean record Removed:

--- src/GUI/MainInterface.java
+++ src/GUI/MainInterface.java
@@ -59,6 +59,7 @@ public class MainInterface extends JFrame implements ActionListener {
         test[0].setFirstName("Chris");
         test[0].setLastName("Niesel");
         test[0].setEmployeeNumber(10001);
+        test[0].setManager(true);
         test[0].setPassword("A94A8FE5CCB19BA61C4C0873D391E987982FBBD3");
     }
 
